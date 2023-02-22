92478bcbe8c2e0cf583ad974519e76995b4cfa6d
Dalton <n7dalton@gmail.com>
2023-02-22 08:55:14 -0600

    GIGGITY

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace Hello_World_Lumpkin_Dalton
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //asks for first name
+            Console.WriteLine("What is your first name?");
+
+            //assigns answer to the string firstname
+            string firstname = Console.ReadLine();
+
+            //asks for last name
+            Console.WriteLine("and your last name?");
+
+            //assigns answer to the string last name 
+            string lastname = Console.ReadLine();
+
+            //says they are weird
+            Console.WriteLine($"So your name is {firstname} {lastname}? weirdo.");
         }
     }
 }
