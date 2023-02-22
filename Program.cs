19bfda26fc2f643ccd6371efadc7151fd6d71afb
Dalton <n7dalton@gmail.com>
2023-02-22 08:46:31 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_World_Lumpkin_Dalton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
